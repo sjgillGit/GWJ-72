8b2a061ea46e092e5d33839c44f4b146a7c7a024
tmdragon2 <133560385+tmdragon2@users.noreply.github.com>
2024-08-12 20:54:28 -0400

    Reapply "Reapply "uodad""
    This reverts commit 30dcfdb4b8b74e93f2e5cf6758c974ea3f019de9.

--- Main/Player/CharacterBody3D.gd
+++ Main/Player/CharacterBody3D.gd
@@ -5,7 +5,7 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
+@onready var Root := $Rootcamera
 
 
 func _physics_process(delta):
@@ -17,7 +17,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY 
 	var input_dir = Input.get_vector("Input_left", "Input_right", "Input_up", "Input_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (Root.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED

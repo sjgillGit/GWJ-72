f5817ce75121c6c56aba4f15731cf6be33b271cd
neHek <kokarev97@inbox.ru>
2024-08-18 14:29:14 -0400

    Rolled the mess back

--- Scripts/VolumeSlider.gd
+++ Scripts/VolumeSlider.gd
@@ -5,7 +5,6 @@ extends HSlider
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	value = db_to_linear(AudioServer.get_bus_volume_db(_bus))
-	$"../Music".play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

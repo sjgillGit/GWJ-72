af5f214b8c495e964f166e4147094999708dd4a8
neHek <kokarev97@inbox.ru>
2024-08-18 14:28:37 -0400

    Revert "Trumasterdragon2/playercontroller (#5)"
    This reverts commit 54360c56154cfe0c5de876674f73b13b55629206.

--- Interactable.gd
+++
@@ -1,8 +0,0 @@
-extends CollisionObject3D
-class_name Interactable
-
-@export var Prompt_message = "Interact"
-
-
-func interact(body):
-	print(body.name,"Touched " , name)
--- Main/Player/CharacterBody3D.gd
+++ Main/Player/CharacterBody3D.gd
@@ -5,17 +5,19 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-@onready var Root := $Rootcamera
-@onready var Item := $"."
-func _process(delta: float) -> void:
-		interact(Item)
+
+
+
 func _physics_process(delta):
+
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+
+
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY 
 	var input_dir = Input.get_vector("Input_left", "Input_right", "Input_up", "Input_down")
-	var direction = (Root.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -24,8 +26,3 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
-
-func interact(Item):
-	if Input.is_action_just_pressed("Mouse_1"):
-		
-		pass
